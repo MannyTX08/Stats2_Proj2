c13f07e353a25dbebfa720a569a7e82f2cba72fc
ArturoCasillas <nerano7g@gmail.com>
2018-04-16 13:45:15 -0500

    Finalize exp_data and ACKaggle

--- LogitWfactors.R
+++ LogitWfactors.R
@@ -3,14 +3,14 @@
 ##########
 
 # split the training data into a secondary test (not Kaggle)
-set.seed(100) # set seed so that same sample can be reproduced in future
+set.seed(200) # set seed so that same sample can be reproduced in future
 
 # now selecting 75% of data as sample from total 'n' rows of the data  
 sample <- sample.int(n=nrow(train1), size=floor(.75*nrow(train1)), replace=FALSE)
 
 # subset the data using the sample integer vector created above
-train3 <- train1[sample, !(names(train1) %in% c("Age","Sibch", "Parch", "Embarked"))]
-test3  <- train1[-sample, !(names(test1) %in% c("Age","Sibch", "Parch", "Embarked"))]
+train3 <- train1[ !(names(train1) %in% c("Age","SibSp", "Parch", "Embarked"))]
+test3  <- test1[ !(names(test1) %in% c("Age","SibSp", "Parch", "Embarked"))]
 str(train3)
 str(test3)
 
@@ -20,6 +20,113 @@ summary(TitanicLog2)
 
 c=train1$Survived
 p=predict(TitanicLog2, newdata=test3, type='response')
+ACkaggle<-cbind.data.frame("PassengerID"=test$PassengerId, "Survived"=round(p))
+write.csv(ACkaggle, file="~/ACKaggle.csv", row.names = FALSE)
+##  best so far: 0.7790
+
+
 cv=cv.glm(data=train3, glmfit=TitanicLog2, K=5)
 #ls(cv)
 cv["delta"]
+
+
+################################################################
+
+titanic_logis<-function(incvars='.',excvars='',mnames){
+  
+  train3 <- train1[ !(names(train1) %in% excvars)]
+  test3  <- test1[ !(names(test1) %in% exvars)]
+}
+
+################################################################
+
+set.seed(200) # set seed so that same sample can be reproduced in future
+
+#  Note, comment some of these out so that the model is not overspecified
+str(train1)
+train1$Male = (train1$Sex == "male")*1
+train1$Female = (train1$Sex == "female") * 1
+train1$Class1 = (train1$Pclass == "1")*1
+train1$Class2 = (train1$Pclass == "2")*1
+train1$Class3 = (train1$Pclass == "3")*1
+train1$PortC = (train1$Embarked == "C")*1
+train1$PortQ = (train1$Embarked == "Q")*1
+train1$PortS = (train1$Embarked == "S")*1
+train1$Child = (train1$AgeBin == "6 or less")*1
+train1$Adult= (train1$AgeBin == "(7 - 63]")*1
+train1$Senior = (train1$AgeBin == "Over 63")*1
+#lapply(levels(train1$Title), function(x) paste("train1", x, sep="$"))
+train1$Master = (train1$Title == "Master")*1
+train1$Miss  = (train1$Title == "Miss")*1
+train1$Mr    = (train1$Title == "Mr")*1
+train1$Mrs   = (train1$Title == "Mrs")*1
+train1$uncommon= (train1$Title == "uncommon")*1
+
+table(	train1$Male, train1$Sex )
+table(	train1$Female, train1$Sex )
+table(	train1$Class1, train1$Pclass)
+table(	train1$Class2, train1$Pclass )
+table(	train1$Class3, train1$Pclass 	)
+table(	train1$PortC, train1$Embarked 	)
+table(	train1$PortQ, train1$Embarked 	)
+table(	train1$PortS, train1$Embarked 	)
+table(	train1$Child, train1$AgeBin	)
+table(	train1$Senior, train1$AgeBin 	)
+table(	train1$Adult, train1$AgeBin 	)
+  table(	train1$Master, train1$Title )
+  table(	train1$Miss, train1$Title 	)
+  table(	train1$Mr, train1$Title )
+  table(	train1$Mrs, train1$Title )
+  table(	train1$uncommon, train1$Title	)
+  
+  
+test1$Male = (test1$Sex == "male")*1
+test1$Female = (test1$Sex == "female") * 1
+test1$Class1 = (test1$Pclass == "1")*1
+test1$Class2 = (test1$Pclass == "2")*1
+test1$Class3 = (test1$Pclass == "3")*1
+test1$PortC = (test1$Embarked == "C")*1
+test1$PortQ = (test1$Embarked == "Q")*1
+test1$PortS = (test1$Embarked == "S")*1
+test1$Child = (test1$AgeBin == "6 or less")*1
+test1$Adult= (test1$AgeBin == "(7 - 63]")*1
+test1$Senior = (test1$AgeBin == "Over 63")*1
+#lapply(levels(test1$Title), function(x) paste("test1", x, sep="$"))
+test1$Master = (test1$Title == "Master")*1
+test1$Miss  = (test1$Title == "Miss")*1
+test1$Mr    = (test1$Title == "Mr")*1
+test1$Mrs   = (test1$Title == "Mrs")*1
+test1$uncommon= (test1$Title == "uncommon")*1
+  
+  
+  Xy<-cbind.data.frame( train1[, !(names(train1) %in% c("Pclass", "Sex", "Embarked", "Title", "AgeBin", "Survived"))],
+                        "Survived"=train1[, "Survived"]  )
+#  testglm<-bestglm(Xy, family = binomial(link='logit'), IC="CV")
+  X<-train1[, !(names(train1) %in% c("Pclass", "Sex", "Embarked", "Title", "AgeBin", "Survived"))]
+  y<-train1[, "Survived"]
+#  testglm2<-glmnet(x=as.matrix(X), y=as.factor(y), family="binomial")
+# ls(testglm2)  
+# summary(testglm2)
+# testglm2$beta
+testglm3<-cv.glmnet(x=as.matrix(X), y, type.measure = "mse")
+
+# testglm4<-glmnet(x=as.matrix(X), y=as.factor(y), family="binomial", lambda=testglm3$lambda)
+# ls(testglm4)  
+# summary(testglm4)
+plot(testglm3)
+
+# testglm4$df
+coef(testglm3, s="lambda.min")
+# testglm4$lambda
+T1<-test1[, !(names(test1) %in% c("Pclass", "Sex", "Embarked", "Title", "AgeBin", "Survived"))]
+p3<-predict(testglm3, as.matrix(T1), s="lambda.min")
+str(X)
+str(T1)
+str(p3)
+
+ACkaggle<-cbind.data.frame("PassengerID"=test$PassengerId, "Survived"=round(p3))
+names(ACkaggle)<-c("PassengerID", "Survived")
+write.csv(ACkaggle, file="~/ACKaggle2.csv", row.names = FALSE)
+## Kaggle score of 0.78947
+
+
--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -1,113 +1,251 @@
 par(mfrow=c(1,1))
+par(xpd=NA)
+#par(oma=c(3.7,0,0,0))
+#par(mar=c(5, 4, 4, 2) + 0.1)
+
+############################
+## FUNCTIONS
+############################
+
+# Barplot Percentages
+
+summ.percent.by<-function(vec1, vec2, main="Percents", xname='vec1', yname='vec2'){
+  par(xpd=NA)
+  par(mar=c(4.5, 4, 4, 2))
+  #par(oma=c(4.6, 0, 0, 0))
+   # vec1=train$Pclass
+   # vec2=train$Survived
+   # main="Percents"
+t<-table(vec1, vec2, dnn=c(xname, yname))
+print(t)
+t2<-t/apply(t, 1, sum)
+barplot(t(t2), col=c('grey', 'lightblue'), legend.text=c('0=Did not Survive','1=Survived'), 
+        main=main, args.legend = list(x="bottom", horiz=TRUE, inset=c(0, -0.3)))
+print(t2)
+par(mar=c(5, 4, 4, 2) + 0.1)
+#par(mar=c(1, 3, 3, 2) + 0.1)
+                                      }
+summ.percent.by(train$Pclass, train$Survived, main="Percent Survived by Class", 
+                xname='Passenger Class', yname="Survived")
+
+
+
+# Dual Histograms
+
+hist.by<-function(vec1, vec2, main="Percents", ymax=25, breaks=breaks, xname='vec1', yname='vec2'){
+  par(mfrow=c(2,1))
+  
+  # vec1=train$Fare
+  # vec2=train$Survived
+  # main="Percents"
+  # xname='vec1'
+  # yname='vec2'
+  # ymax=25
+  
+  vec1_1=vec1[vec2==1]
+  vec1_2=vec1[vec2==0]
+  
+  t<-table(vec1, vec2, dnn=c(xname, yname))
+  xmin=min(vec1)
+  xmax=max(vec1)
+  ymax=ymax
+  
+  breaks2=seq(xmin, xmax, length.out = breaks)
+  
+  par(mar=c(0.2,5,3,3))
+  hist(vec1_1 , main=main , xlim=c(xmin,xmax), ylab="Freq. Survived", xlab="", ylim=c(0,ymax) , xaxt="n", 
+       las=1 , col="lightblue", breaks=breaks2)
+  
+  legend("topright", c('Survived', 'Did Not Survive'), col=c('lightblue','tomato3') , pch = 15)
+  
+  par(mar=c(5,5,0.2,3))
+  hist(vec1_2 , main= '', xlim=c(xmin,xmax), ylab="Freq. not Survived", xlab=xname, ylim=c(ymax,0) , 
+       las=1 , col="tomato3"  , breaks=breaks2)
+  
+ par(mfrow=c(1,1))
+  par(mar=c(5, 4, 4, 2) + 0.1)
+                                                                                  }
+# test
+hist.by(train$Fare[train$Fare < 150], train$Survived[train$Fare < 150], 
+        main="Histograms of Fare by Survival", ymax=300, breaks=20, 
+        xname='Fare', yname="Survived")
+
+############################
+## PASSENGERID
+############################
 
 summary(train[,"PassengerId"])
 summary(test[,"PassengerId"])
 length(unique(train[,"PassengerId"]))
 length(unique(test[,"PassengerId"]))
 
+############################
+## SURVIVED
+############################
+
 summary(train[,"Survived"])
+
+
+############################
+## PCLASS
+############################
 
 summary(train[,"Pclass"])
 summary(test[,"Pclass"])
-t<-table(train$Pclass, train$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
 
-summary(train[,"Name"])
-summary(test[,"Name"])
+summ.percent.by(train$Pclass, train$Survived, main="Percent Survived by Class", 
+                xname='Passenger Class', yname="Survived")
+
+############################
+## Name
+############################
+
+summary(train[,"Name"])[1:30]
+summary(test[,"Name"])[1:30]
 length(unique(train[,"Name"]))
 length(unique(test[,"Name"]))
 
 
+############################
+## TITLE
+############################
+
+summary(train1[,"Title"])
+summary(test1[,"Title"])
+
+summ.percent.by(train1$Title, train1$Survived, main="Proportion Survived by Title", 
+                xname='Passenger Title', yname="Survived")
+
+table(train1$Title[!(train1$AgeBin %in% c("(0,10]", "(10,20]"))], train1$AgeBin[!(train1$AgeBin %in% c("(0,10]", "(10,20]"))])
+
+summ.percent.by(train1$Title[!(train1$AgeBin %in% c("(0,10]", "(10,20]"))], 
+                train1$Survived[!(train1$AgeBin %in% c("(0,10]", "(10,20]"))],
+                main="Proportion Survived by Title, Age > 20", 
+                xname='Passenger Title', yname="Survived")
+
+summ.percent.by(train1$Title[!(train1$AgeBin %in% c("(0,10]", "(10,20]","unknown"))], 
+                train1$Survived[!(train1$AgeBin %in% c("(0,10]", "(10,20]", "unknown"))],
+                main="Proportion Survived by Title, Age > 20 and not Unknown", 
+                xname='Passenger Title', yname="Survived")
+
+############################
+## SEX
+############################
+
 summary(train[,"Sex"])
 summary(test[,"Sex"])
 
-summary(train[,"Age"])
-summary(test[,"Age"])
-hist(train$Age, breaks=seq(0,100, by=5))
-sort(train$Age)[1:30]
+summ.percent.by(train$Sex, train$Survived, main="Percent Survived by Sex", 
+                xname='Passenger Sex', yname="Survived")
+
+############################
+## AGEBIN
+############################
 
 summary(train1[,"AgeBin"])
 summary(test1[,"AgeBin"])
-t<-table(train1$AgeBin, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+
+# FareBin<-cut(train$Fare, seq(0, 60, by=2))
+# l<-levels(FareBin)[-(length(levels(FareBin)))]
+# levels(FareBin)<-c(l, '> $58')
+
+summ.percent.by(train1$AgeBin, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
 
 AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=5)))
-#full$AgeBin[10:35]
 l<-levels(AgeBin2)[-(length(levels(AgeBin2)))]
-#l
-#    replace <NA> with 'unknown'
 levels(AgeBin2)<-c(l, 'unknown')
-t<-table(AgeBin2, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
 
 AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=6)))
-#full$AgeBin[10:35]
 l<-levels(AgeBin2)[-(length(levels(AgeBin2)))]
-#l
-#    replace <NA> with 'unknown'
 levels(AgeBin2)<-c(l, 'unknown')
-t<-table(AgeBin2, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
 
 AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=9)))
-#full$AgeBin[10:35]
 l<-levels(AgeBin2)[-(length(levels(AgeBin2)))]
-#l
-#    replace <NA> with 'unknown'
 levels(AgeBin2)<-c(l, 'unknown')
-t<-table(AgeBin2, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
+
+AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=10)))
+l<-levels(AgeBin2)[-(length(levels(AgeBin2)))]
+levels(AgeBin2)<-c(l, 'unknown')
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
 
 AgeBin3<-addNA(cut(train$Age, seq(0, 90, by=15)))
-#full$AgeBin[10:35]
-l<-levels(AgeBin3)[-(length(levels(AgeBin3)))]
-#l
-#    replace <NA> with 'unknown'
+l<-levels(AgeBin3)[-(length(levels(AgeBin2)))]
 levels(AgeBin3)<-c(l, 'unknown')
-t<-table(AgeBin3, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+
+summ.percent.by(AgeBin3, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
 
 AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=6)))
-#full$AgeBin[10:35]
-#l<-levels(AgeBin2)[-(length(levels(AgeBin2)))]
-#l
-#    replace <NA> with 'unknown'
-levels(AgeBin2)<-c("6 or less", rep("7 - 60", 9), rep("60+", 5), "unknown")
-t<-table(AgeBin2, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+levels(AgeBin2)<-c("6 or less", rep("7 - 60", 9), rep("Over 60", 5), "unknown")
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
 
 AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=6)))
-#full$AgeBin[10:35]
-#l<-levels(AgeBin2)[-(length(levels(AgeBin2)))]
-#l
-#    replace <NA> with 'unknown'
 levels(AgeBin2)<-c("6 or less", rep("7 - 65", 10), rep("66+", 4), "unknown")
-t<-table(AgeBin2, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
+
+AgeBin2<-addNA(cut(train$Age, seq(0, 90, by=3)))
+levels(AgeBin2)<-c(rep("6 or less", 2), rep("(7 - 63]", 19), rep("Over 63", 9), "unknown")
+
+summ.percent.by(AgeBin2, train1$Survived, main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger', yname="Survived")
+
+# After imputation
+AgeBin3<-cut(train1$Age, seq(0, 90, by=3))
+levels(AgeBin3)<-c(rep("6 or less", 2), rep("(7 - 63]", 19), rep("Over 63", 9))
+
+summ.percent.by(AgeBin3, 
+                train1$Survived, 
+                main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger (after imputation)', yname="Survived")
+
+AgeBin3<-cut(train1$Age, seq(0, 90, by=3))
+levels(AgeBin3)<-c(rep("6 or less", 2), rep("(7 - 60]", 18), rep("Over 60", 10))
+
+summ.percent.by(AgeBin3, 
+                train1$Survived, 
+                main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger (after imputation)', yname="Survived")
+
+AgeBin3<-cut(train1$Age, seq(0, 90, by=6))
+#levels(AgeBin3)<-c(rep("6 or less", 2), rep("(7 - 63]", 19), rep("Over 63", 9))
+
+summ.percent.by(AgeBin3, 
+                train1$Survived, 
+                main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger (after imputation)', yname="Survived")
+
+AgeBin3<-cut(train1$Age, seq(0, 90, by=10))
+#levels(AgeBin3)<-c(rep("6 or less", 2), rep("(7 - 63]", 19), rep("Over 63", 9))
+
+summ.percent.by(AgeBin3, 
+                train1$Survived, 
+                main="Proportion Survived by AgeBin", 
+                xname='Age of Passenger (after imputation)', yname="Survived")
+
 
 hist(train$Age[train$Survived == 1], breaks=seq(0,100, by=5))
 hist(train$Age[train$Survived == 0], breaks=seq(0,100, by=5))
@@ -118,84 +256,158 @@ t2<-t/apply(t, 1, sum)
 t2
 barplot(t(t2))
 
+############################
+## AGE BEFORE IMPUTATION
+############################
+
+summary(train[,"Age"])
+summary(test[,"Age"])
+
+hist.by(train$Age[!is.na(train$Age)], train$Survived[!is.na(train$Age)], 
+        main="Histograms of Age by Survival", ymax=100, breaks=16, 
+        xname='Age (after imputation)', yname="Survived")
+
+hist.by(train$Age[train1$Age >= 45 & !is.na(train$Age)], 
+        train$Survived[train1$Age >= 45 & !is.na(train$Age)], 
+        main="Histograms of Age by Survival", ymax=25, breaks=12, 
+        xname='Age (after imputation)', yname="Survived")
+
+hist.by(train$Age[train1$Age >= 45 & !is.na(train$Age)], 
+        train$Survived[train1$Age >= 45 & !is.na(train$Age)],
+        main="Histograms of Age by Survival", ymax=15, breaks=20, 
+        xname='Age (after imputation)', yname="Survived")
+
+############################
+## AGE AFTE IMPUTATION
+############################
 #After imputation
 summary(train1[,"Age"])
 summary(test1[,"Age"])
 
+hist.by(train1$Age, train$Survived, 
+        main="Histograms of Age by Survival", ymax=150, breaks=15, 
+        xname='Age (after imputation)', yname="Survived")
+
+hist.by(train1$Age[train1$Age >= 45], train$Survived[train1$Age >= 45], 
+        main="Histograms of Age by Survival", ymax=30, breaks=15, 
+        xname='Age (after imputation)', yname="Survived")
+
+hist.by(train1$Age[train1$Age >= 45], train$Survived[train1$Age >= 45], 
+        main="Histograms of Age by Survival", ymax=20, breaks=16, 
+        xname='Age (after imputation)', yname="Survived")
+
+hist.by(train1$Age[train1$Age >= 45], train$Survived[train1$Age >= 45], 
+        main="Histograms of Age by Survival", ymax=15, breaks=20, 
+        xname='Age (after imputation)', yname="Survived")
+
+############################
+## FARE
+############################
+
 summary(train[,"Fare"])
 summary(test[,"Fare"])
 
-FareBin<-cut(train$Age, seq(0, 60, by=6))
-#FareBin[10:35]
-l<-levels(FareBin)[-(length(levels(FareBin)))]
-#l
-#    replace <NA> with 'unknown'
-levels(FareBin)<-c(l, '> $55')
-t<-table(FareBin, train$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+#   Dual Histograms
 
-FareBin<-cut(train$Age, seq(0, 60, by=5))
-#FareBin[10:35]
-l<-levels(FareBin)[-(length(levels(FareBin)))]
-#l
-#    replace <NA> with 'unknown'
-levels(FareBin)<-c(l, '> $55')
-t<-table(FareBin, train$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+hist.by(train$Fare[train$Fare < 150], train$Survived[train$Fare < 150], 
+        main="Histograms of Fare by Survival", ymax=400, breaks=10, 
+        xname='Fare', yname="Survived")
 
-FareBin<-cut(train$Age, seq(0, 60, by=4))
-#FareBin[10:35]
+hist.by(train$Fare[train$Fare < 150], train$Survived[train$Fare < 150], 
+        main="Histograms of Fare by Survival", ymax=300, breaks=30, 
+        xname='Fare', yname="Survived")
+
+#   Percentages by Fare
+
+FareBin<-cut(train$Fare, seq(0, 60, by=2))
 l<-levels(FareBin)[-(length(levels(FareBin)))]
-#l
-#    replace <NA> with 'unknown'
+levels(FareBin)<-c(l, '> $58')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+
+FareBin<-cut(train$Fare, seq(0, 60, by=3))
+l<-levels(FareBin)[-(length(levels(FareBin)))]
+levels(FareBin)<-c(l, '> $57')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+
+FareBin<-cut(train$Fare, seq(0, 60, by=4))
+l<-levels(FareBin)[-(length(levels(FareBin)))]
 levels(FareBin)<-c(l, '> $56')
-t<-table(FareBin, train$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
 
-FareBin<-cut(train$Age, seq(0, 60, by=3))
-#FareBin[10:35]
-l<-levels(FareBin)[-(length(levels(FareBin)))]
-#l
-#    replace <NA> with 'unknown'
-levels(FareBin)<-c(l, '> $57')
-t<-table(FareBin, train$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
 
-FareBin<-cut(train$Age, seq(0, 63, by=7))
-#FareBin[10:35]
+FareBin<-cut(train$Fare, seq(0, 60, by=5))
 l<-levels(FareBin)[-(length(levels(FareBin)))]
-#l
-#    replace <NA> with 'unknown'
-levels(FareBin)<-c(l, '> $57')
-t<-table(FareBin, train$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
+levels(FareBin)<-c(l, '> $55')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+
+FareBin<-cut(train$Fare, seq(0, 60, by=6))
+l<-levels(FareBin)[-(length(levels(FareBin)))]
+levels(FareBin)<-c(l, '> $54')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+
+FareBin<-cut(train$Fare, seq(0, 63, by=7))
+l<-levels(FareBin)[-(length(levels(FareBin)))]
+levels(FareBin)<-c(l, '> $56')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+
+FareBin<-cut(train$Fare, seq(0, 72, by=8))
+l<-levels(FareBin)[-(length(levels(FareBin)))]
+levels(FareBin)<-c(l, '> $64')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+FareBin<-cut(train$Fare, seq(0, 64, by=9))
+l<-levels(FareBin)[-(length(levels(FareBin)))]
+levels(FareBin)<-c(l, '> $63')
+
+summ.percent.by(FareBin, train1$Survived, main="Proportion Survived by Fare", 
+                xname='Ticket Price', yname="Survived")
+
+############################
+## CABIN
+############################
 
 summary(train[,"Cabin"])[1:30]
 summary(test[,"Cabin"])[1:30]
 
+section_train=substr(train[,"Cabin"], 1, 1)
+section_test=substr(test[,"Cabin"], 1, 1)
+
+table(section_train)
+table(section_test)
+
+summ.percent.by(section_train, train1$Survived, main="Proportion Survived by Section", 
+                xname='Cabin Section', yname="Survived")
+
+library('stringr')
+
+sections_train=sapply(train[,"Cabin"], function(x) str_extract_all(x, "[A-z]"))
+unique(sections_train)
+
+############################
+## EMBARKED
+############################
+
 summary(train[,"Embarked"])
 summary(test[,"Embarked"])
 
+summ.percent.by(train1$Embarked, train1$Survived, main="Proportion Survived by Port", 
+                xname='Port of Embarkation', yname="Survived")
 
-summary(train1[,"Title"])
-summary(test1[,"Title"])
-t<-table(train1$Title, train1$Survived)
-t
-t2<-t/apply(t, 1, sum)
-t2
-barplot(t(t2))
